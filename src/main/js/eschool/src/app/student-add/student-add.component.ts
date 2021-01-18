f8de0b5b5ff6825ad8dc2bd45a95575ef94d1418
Raiden <qwedich@gmail.com>
2021-01-19 00:43:45 +0300

    Some correcting details

--- src/main/js/eschool/src/app/student-add/student-add.component.ts
+++ src/main/js/eschool/src/app/student-add/student-add.component.ts
@@ -14,7 +14,7 @@ export class StudentAddComponent implements OnInit {
   firstname = new FormControl('', Validators.required);
   lastname = new FormControl('', Validators.required);
   email = new FormControl('', [Validators.required, Validators.email]);
-  class = new FormControl('', Validators.required);
+  performance = new FormControl('', Validators.required);
 
   student: Student;
 
@@ -30,26 +30,24 @@ export class StudentAddComponent implements OnInit {
   }
 
   private addStudent(){
-    this.studentService.addStudent(this.student).subscribe(data => this.goToList());
+    this.studentService.addStudent(this.student).subscribe(data => window.location.reload());
   }
 
-  goToList(){
-    window.location.reload();
+  getFirstNameErrorMessage(){
+    return this.firstname.hasError('required') ? 'Это обязатльное поле' : '';
   }
 
-  getErrorMessage() {
-    if (this.firstname.hasError('required'))
-      return 'Это обязатльное поле';
+  getLastNameErrorMessage(){
+    return this.lastname.hasError('required') ? 'Это обязатльное поле' : '';
+  }
 
-    if (this.lastname.hasError('required'))
-      return 'Это обязатльное поле';
+  getPerformanceErrorMessage(){
+    return this.performance.hasError('required') ? 'Это обязатльное поле' : '';
+  }
 
+  getEmailErrorMessage() {
     if (this.email.hasError('required'))
       return 'Это обязатльное поле';
-
-    if (this.class.hasError('required'))
-      return 'Это обязатльное поле';
-
 
     return this.email.hasError('email') ? 'Неправильная почта' : '';
   }
