72fa76aacf75a0c20d4f032ab4e56e867864efd4
Raiden <qwedich@gmail.com>
2021-01-18 09:44:40 +0300

    Studnet Rest Controller

---
+++ src/main/java/kz/ali/eSchool/controllers/StudentController.java
@@ -0,0 +1,32 @@
+package kz.ali.eSchool.controllers;
+
+import kz.ali.eSchool.models.Student;
+import kz.ali.eSchool.services.StudentService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@CrossOrigin
+public class StudentController {
+    @Autowired
+    StudentService studentService;
+
+    @GetMapping("/students")
+    public List<Student> getStudents(Model model){
+        return studentService.getAll();
+    }
+
+    @PostMapping("/add")
+    public void addStudent(@RequestBody Student student){
+        studentService.addStudent(student);
+    }
+
+    @DeleteMapping("/delete/{id}")
+    public void deleteStudent(@PathVariable Long id){
+        studentService.deleteStudent(id);
+    }
+
+}
